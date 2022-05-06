dd5b61e2dfb531a54fd0962d3af01300c95efe77
Wilbert Chen <wilbertchen2001@gmail.com>
2022-05-06 14:49:43 +0700

    feat: add movie catalogue files

---
+++ specs/helpers/testFactories.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import FavoriteMovieIdb from '../../src/scripts/data/favorite-movie-idb';
+import LikeButtonPresenter from '../../src/scripts/utils/like-button-presenter';
+
+const createLikeButtonPresenterWithMovie = async (movie) => {
+  await LikeButtonPresenter.init({
+    likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favoriteMovies: FavoriteMovieIdb,
+    movie,
+  });
+};
+
+export { createLikeButtonPresenterWithMovie };
---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: 'acaea05d137143b00bbfe84d9bd7796b',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
